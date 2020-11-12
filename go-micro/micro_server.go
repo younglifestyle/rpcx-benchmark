33c090b520966d5ff561a7b8ee98873e433d22ef
younglifestyle <718800822@qq.com>
2020-11-12 16:51:08 +0800

    增加Go Micro的测试

---
+++ go-micro/micro_server.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+	"strconv"
+	"time"
+
+	micro "github.com/micro/go-micro/v2"
+	pb "github.com/rpcxio/rpcx-benchmark/go-micro/pb"
+	"golang.org/x/net/context"
+)
+
+var (
+	host  string
+	delay int
+)
+
+type Hello struct{}
+
+func (t *Hello) Say(ctx context.Context, args *pb.BenchmarkMessage, reply *pb.BenchmarkMessage) (err error) {
+	s := "OK"
+	var i int32 = 100
+	reply.Field1 = s
+	reply.Field2 = i
+	if delay > 0 {
+		time.Sleep(time.Duration(delay))
+	} else {
+		runtime.Gosched()
+	}
+	return nil
+}
+
+func main() {
+	host, _ = os.LookupEnv("HOST")
+	if host == "" {
+		host = "127.0.0.1:8972"
+	}
+
+	d, _ := os.LookupEnv("DELAY")
+	if d != "" {
+		delay, _ = strconv.Atoi(d)
+	}
+
+	// Create a new service. Optionally include some options here.
+	service := micro.NewService(
+		micro.Name("hello"),
+	)
+
+	// Init will parse the command line flags.
+	service.Init(micro.Address(host))
+
+	// Register handler
+	pb.RegisterHelloHandler(service.Server(), new(Hello))
+
+	// Run the server
+	if err := service.Run(); err != nil {
+		fmt.Println(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -4,14 +4,16 @@ go 1.14
 
 require (
 	github.com/gogo/protobuf v1.3.1
+	github.com/golang/protobuf v1.4.1
 	github.com/juju/ratelimit v1.0.1
 	github.com/lesismal/arpc v0.0.0-20201020140159-7cc18174f381
-	github.com/lesismal/arpcext v0.0.0-20201015173936-8eade3a4c744 // indirect
 	github.com/mars9/codec v0.0.0-20160208102151-49676af5c6e0
 	github.com/mattn/go-colorable v0.1.6 // indirect
+	github.com/micro/go-micro/v2 v2.9.1
 	github.com/montanaflynn/stats v0.6.3
 	github.com/smallnest/rpcx v0.0.0-20200604140806-03ce6c620eb8
 	golang.org/x/net v0.0.0-20200602114024-627f9648deb9
 	golang.org/x/sys v0.0.0-20200602225109-6fdc65e7d980 // indirect
-	google.golang.org/grpc v1.29.1
+	google.golang.org/grpc v1.26.0
+	google.golang.org/protobuf v1.23.0
 )
